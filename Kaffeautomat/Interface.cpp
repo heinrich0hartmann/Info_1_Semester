df5dba478b163dea293301d8a44e64a35a6d8f8d
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2021-11-29 13:28:35 +0100

    Create Interface.cpp
    Kaffeeautomat Interface

---
+++ Kaffeautomat/Interface.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char x;
+	system("cls");
+	cout << "Herzlich Wilkommen beim Kaffeautomaten!\nBitte w\x84hlen Sie aus :\n(k) Kaffe\n(e) Espresso\n(s) Service- Mode\n";
+	cin >> x;
+	cout << "Sie haben sich f\x81r die Auswahl " << x << "entschieden\n";
+}
