45260372fc59c8b432382ee8b9e65b0417b05edb
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2021-11-29 12:37:47 +0100

    Update Quelle1.cpp

--- Quelle1.cpp
+++ Quelle1.cpp
@@ -1,4 +1,11 @@
 #include<iostream>
 using namespace std; //Eintrittsfunktion
 int main(void)
-{}
+{
+	int a;
+	int b;
+	b = 36
+		a = b + 6
+		cout << "Hello World a = " << a << endl;
+	return 0;
+}
