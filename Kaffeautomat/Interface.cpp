5bce4d178c8f34189c8cb3a4a0c0d52f3569007c
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2021-11-30 10:22:03 +0100

    Leerzeile

--- Kaffeautomat/Interface.cpp
+++ Kaffeautomat/Interface.cpp
@@ -6,5 +6,5 @@ int main()
 	system("cls");
 	cout << "Herzlich Wilkommen beim Kaffeautomaten!\nBitte w\x84hlen Sie aus :\n(k) Kaffe\n(e) Espresso\n(s) Service- Mode\n";
 	cin >> x;
-	cout << "Sie haben sich f\x81r die Auswahl " << x << "entschieden\n";
+	cout << "Sie haben sich f\x81r die Auswahl " << x << " entschieden\n";
 }
