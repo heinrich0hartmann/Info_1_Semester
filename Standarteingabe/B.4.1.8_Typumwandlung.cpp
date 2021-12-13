4881c81460a6ccdd95e86b192f034adc883a8b50
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2021-12-13 12:38:40 +0100

    B.4.1.7 Typumwandlung Initialisiert

--- Standarteingabe/B.4.1.8_Typumwandlung.cpp
+++ Standarteingabe/B.4.1.8_Typumwandlung.cpp
@@ -9,7 +9,12 @@ double laenge[100.05];
 char zeichen;
 1 = laenge;
 zeichen = laenge;
-cout << laenge << "_" << 1 << "_" << zeichen << endl;
+cout << laenge << " " << 1 << " " << zeichen << endl;
+string messlatte ["777.77"];
+laenge = static_cast<double>(messlatte);
 
+system("pause")
+
+return 0;
 
 ]
