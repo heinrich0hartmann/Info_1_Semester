5f9f3504442520ba9e3ce2f0d0ae200865cad582
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2021-12-13 11:00:01 +0100

    Strings und Konsolenausgabe Initialisieren

--- Standarteingabe/B.4.1.7_Standarteingabe.cpp
+++ Standarteingabe/B.4.1.7_Standarteingabe.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
-#include<string>
-#include<sstream>
+#include<string>		//Für Zeichen
+#include<sstream>		//konvertierung Datentypen
 using namespace std;
 int main(void)
 {
-	cout << "Bitte geben Sie Ihren (Nach)Namen ein" << endl;
+	cout << "Bitte geben Sie Ihren (Nach)Namen ein" << endl;		//Konsolenausgabe
+	string Name;
+	cin >> Name;
+
+	cout << "Bitte geben Sie Ihren Vornamen ein" << endl;
+	string Vorname;
+	cin >> Vorname;
+
+	cout << "Bitte geben Sie Ihren Wohnort ein" << endl;
+	string Wohnort;
+	cin >> Wohnort;
+
+	cout << "Bitte geben Sie Ihre Stra\x223 \be ein" << endl;
 
 
 }
