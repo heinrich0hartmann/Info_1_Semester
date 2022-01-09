a683912f5ffd92d9c9eab5409acc341b5bcdc8a1
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-01-09 18:02:05 +0100

    nicht deklarierte Bezeichner korrigiert

--- Programmierübungen/B.4.3.2_Verzweigungen.cpp
+++ Programmierübungen/B.4.3.2_Verzweigungen.cpp
@@ -7,16 +7,16 @@ int main()
 
 	i = 0; j = -5;
 	if (i++ || j++) ++i;				// Wenn die eingegebene Zahl i=(0) oder j=(-5) entspricht gilt die Bedingung al erfüllt
-	cout << i << ´,´ << j << endl;
+	cout << i << ',' << j << endl;
 	i = 1; j = -5;
 	if (i++ || j++) ++i;				//
-	cout << i << ´,´ << j << endl;
+	cout << i << ',' << j << endl;
 	i = 0; j = -5;
 	if (i++ && j++) ++i;				//
-	cout << i << ´,´ << j << endl;
+	cout << i << ',' << j << endl;
 	i = 1; j = -5;
 	if (i++ && j++) ++i;				//
-	cout << i << ´,´ << j << endl;
+	cout << i << ',' << j << endl;
 
 }
 
