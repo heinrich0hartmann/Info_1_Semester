4a2ef6fab89d173eaee2b3e5f00ac7ca97d3c13a
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-01-09 20:09:46 +0100

    Fertige Kommentare

--- Programmierübungen/B.4.3.2_Verzweigungen.cpp
+++ Programmierübungen/B.4.3.2_Verzweigungen.cpp
@@ -6,16 +6,22 @@ int main()
 	int i, j;
 
 	i = 0; j = -5;
-	if (i++ || j++) ++i;				// Wenn die eingegebene Zahl i=(0) oder j=(-5) entspricht gilt die Bedingung al erfüllt
+	if (i++ || j++) ++i;				// Zuerst weden Die Variablen i,j in den Datentyp bool convertiert, dabei werden alle werte die von 0 abweichen zu true
+										//und alle die =0 sind werden zu false, da beim logischen Oder der Zweite wert nur bearbeitet wird, wenn der erste false ist werden
+										//hier alle bearbeitet d.h. (0=false ODER -5=true) und i anschließend +1 also i+1+1 und j+1 = i:2 und j:-4
+										//i++ bedeutet Inkrementierung nach Rechnung, ++i Inkrementierung vor Rechnung
 	cout << i << ',' << j << endl;
 	i = 1; j = -5;
-	if (i++ || j++) ++i;				//
+	if (i++ || j++) ++i;				//Da die erste Bedingung i (1) als true gilt wird die zweite nicht bearbeitet d.h. (1=true ODER xxxx) und es wird i+1+1 gerechnet und
+										//j bleibt gleich, also i:3 und j:5
 	cout << i << ',' << j << endl;
 	i = 0; j = -5;
-	if (i++ && j++) ++i;				//
+	if (i++ && j++) ++i;				//Hier sind die Variablen durch das Logische UND verknüpft, es müssen also beide Bedingungen erfüllt sein (true / true), da jedoch i schon als 
+										//bool false gilt wird abgebrochen und nur i +1 Inkremetiert, da die Bedingung nicht erfüllt wurdr wird auch i nicht nachträglich nochmal Inkrementiert.
+										//i:1 und j:-5
 	cout << i << ',' << j << endl;
 	i = 1; j = -5;
-	if (i++ && j++) ++i;				//
+	if (i++ && j++) ++i;				//Die Bedingungen Boolsch true und true sind (1/1) werden auch beide Inkrementiert und anschließend noch i um eins Inkrementiert i:3 und j:-4
 	cout << i << ',' << j << endl;
 
 }
