dd28a1b7fa634a2a51fe2f9681db618eb09f61ad
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-01-09 17:58:53 +0100

    Korrektur Synatx

--- Programmierübungen/B.4.3.2_Verzweigungen.cpp
+++ Programmierübungen/B.4.3.2_Verzweigungen.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main()
-[
-int i, j;
+{
+	int i, j;
 
-i = 0; j = -5;
-if (i++ || j++) ++i;				//
-cout << i << `,´ << j << endl;
-i = 1; j = -5;
-if (i++ || j++) ++i;				//
-cout << i << `,´ << j << endl;
-i = 0; j = -5;
-if (i++ && j++) ++i;				//
-cout << i << `,´ << j << endl;
-i = 1; j = -5;
-if (i++ && j++) ++i;				//
-cout << i << `,` << j << endl;
+	i = 0; j = -5;
+	if (i++ || j++) ++i;				// Wenn die eingegebene Zahl i=(0) oder j=(-5) entspricht gilt die Bedingung al erfüllt
+	cout << i << ´,´ << j << endl;
+	i = 1; j = -5;
+	if (i++ || j++) ++i;				//
+	cout << i << ´,´ << j << endl;
+	i = 0; j = -5;
+	if (i++ && j++) ++i;				//
+	cout << i << ´,´ << j << endl;
+	i = 1; j = -5;
+	if (i++ && j++) ++i;				//
+	cout << i << ´,´ << j << endl;
 
-]
+}
+
