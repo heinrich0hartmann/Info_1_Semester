d2cbb75e2fedbfd6cfdb766099265ca0f293e873
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-01-09 21:11:59 +0100

    Euklid Struktur

---
+++ Programmierübungen/B.4.4.1_Euklid.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int ggt (int x, int y);
+
+main() 
+{
+	int x, y;
+	cout << "Der Euklidische Algorithmus";
+	cout << "Gesucht wird der groeste gemeinsame Teiler von zwei positiven ganzen Zahlen \x\y";
+	cout << "Zahl_1";
+	cin >> x;
+	cout << "Zahl_2";
+	cin >> y;
+	cout << "Groesster gemeinsamer Teiler ist: " << ggt(x, y);
+ }
+
+int ggt(int x, int y)
+{
+	int r;
+	do
+	{
+		if (x < y)
+		{
+			r = x;
+			x = y;
+			y = r;
+}
+		r = x % y;
+		if (r != 0)
+		{
+			x = y;
+			y = r;
+		}	
+	} while (r!=0);
+	return(y);
+}
+
+ 
+
