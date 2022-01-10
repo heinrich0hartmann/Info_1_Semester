3dfc8ad5dbccee6a63bb7faad338e55dfc765c9c
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-01-10 10:17:21 +0100

    Aufgabe Schleifen

---
+++ Programmierübungen/B.4.4.2_Schleifen.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i, k, n;
+
+	i = 101, k = 5, n = 10;
+	while (i > 0) k = 2 * k;		//
+
+	i = 101, k = 5, n = 10;
+	while (i! = 0) i = i - 2;		//
+
+	i = 101, k = 5, n = 10;
+	while (n! = i)					//
+	{
+		++i;
+		n = 2 * i;
+	}
+}
