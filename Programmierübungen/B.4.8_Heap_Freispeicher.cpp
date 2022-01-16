d3a061460c67c739bdc79f66493b0c287bc8158a
heinrich0hartmann <heinrich.hartmann@stud.hawk.de>
2022-01-16 20:19:41 +0100

    Initialisierung Heap

---
+++ Programmierübungen/B.4.8_Heap_Freispeicher.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	double* werte;
+	int n;
+	cout << "Wie viele Messwerte wollen Sie im Heap speichern?" << endl; //Abfrage Werte
+	cout << "Eingabe: ";
+	cin << n;
+	werte = new double[n]; //Initialisierung Werte
+	if (werte == 0)
+	{
+		cout << endl << "Nicht gen\x9Agend Speicherplatz vorhanden" << endl;
+		system("pause");
+		return 0;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Bitte geben Sie einen Ihrer Messwerte ein: " << endl;
+		cin >> *(werte + i);
+	}
+	cout << endl << "Ihre Messwerte: " << endl;
+	for (int i = 0; i < n; ++i)
+	{
+		cout << *(werte + i) << endl;
+	}
+	double Mittelwert;
+	double SummeM = 0;
+
+	for (int i = 0; i != n; i++)
+	{
+		SummeM += *(werte + i);
+	}
+	Mittelwert = SummeM / n;
+	cout << endl << "Mittelwert: " << Mittelwert << "m" << endl;
+	
+	double Varianz;
+	double SummeV = 0;
+
+	for (int i = 0; i != n; i++)
+	{
+		SummeV += pow(((werte[i]) - Mittelwert), 2); 
+	}
+	Varianz = SummeV / (n - 1);
+	cout << endl << "Varianz: " << Varianz << "Meter" << endl; 
+
+	delete[] werte; //Befehle alle Heap Dateien Löschen
+
+}
